d52c22ac3c3df1eb08c6cef272fbad0b0aed5817
Handshy <Ian.Handshy@ergodyne.com>
2017-05-06 13:56:41 -0500

    start getall tweets branch

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Ian Handshy
-## Project: 2 - Twitter Bot: Jack Robo Burton
+## Project: 3 - Twitter Bot Enriched Features: Jack Robo Burton
 
+### Features added: CRUD functions accessing Twitter's API, third party authentication
+
+
+### What Bot Does
 Jack Burton is possibly the greatest hero to ever walk the Earth at least in his own mind. He plays the main role in Big Trouble in Little China, an action/comedy released in 1986. It's a classic. Jack is an anti-hero, and mostly provides comic relief.
 
 As such, Jack in bot form takes headlines from The Onion on twitter and reposts them as advice, from the greatest action hero ever.
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var Twit = require('twit');
+var TwitterBot = require('node-twitterbot').TwitterBot;
+var botCreds = {
+ consumer_key: '2ep9JxmzJZWRFsHBMEUjhcR0m',
+ consumer_secret: '8SiofFEtnWsA7LStjCbXV7xsJh2wXGCr3yqCXR0dTqY3ejTbpw',
+ access_token: '851970802543144961-IuygBxGwSjUwKokUyArh2yX4m8fEvCn',
+ access_token_secret: 'SKv5ZNUZEe5BB817TravMp3W3Rii3fhoh0oILNVfKWC6K',
+ timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
+};
+var T = new Twit(botCreds);
+
 
 router.get('/', function(req, res, next) {
-	
-	res.render('index', { title: 'Jack Robo Burton', description: 'You know what ol\' Jack Burton always says at a time like this?'});
+	// need to wrap this in a function that queries existing posts on jack b
+	// then stores them in an obj, and write the object into the templete
+	// fist do this by query and log to console
+	// then write to the index template.
+	// looking at API, I think I'll only be able to delete tweets.
+	// Jack user id 851970802543144961
+	T.get('statuses/user_timeline', { user_id: '851970802543144961', count: 2}, function(err, data, response) {
+      if (err) {
+          return next(err);
+      }
+          
+      if (data) {
+           
+		// res.render('index', { title: 'Jack Robo Burton', description: 'You know what ol\' Jack Burton always says at a time like this?'});
+      
+      }
+
+});
+
+
 
 });
 
